9c71dd62d8ebc915976b9ba05e328d36f6984e26
zahed khan <zahed3795@icloud.com>
2020-11-09 01:26:42 -0500

    Update installation.bat

--- installation.bat
+++ installation.bat
@@ -1,6 +1,13 @@
 @ECHO OFF
 python setup.py install
 pip install -r requirements.txt
+py -m pip install virtualenvwrapper-win --force-reinstall --user
+py -m pip --upgrade pip
+pip install -e . --upgrade --no-cache-dir --progress-bar off
+py -m venv venv
+call venv\\Scripts\\activate
+python setup.py install
+pip install -r requirements.txt
 zahed install chrome -l
 zahed install gecko -l
 zahed install edge -l
