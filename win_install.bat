740c77ace6f8d84bcd85a25a0a32b9c7f35aad1f
zahed <47490281+zahed3795@users.noreply.github.com>
2020-11-12 08:14:32 -0500

    Update win_install.bat

--- win_install.bat
+++ win_install.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
-py -m pip --upgrade pip
-pip install -e . --upgrade --no-cache-dir --progress-bar off
 python setup.py install
 pip install -r requirements.txt
+py -m pip --upgrade pip
+pip install -e . --upgrade --no-cache-dir --progress-bar off
 zahed install chrome -l
 zahed install gecko -l
 zahed install edge -l
